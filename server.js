dc586e7b2d58e06c8832c5713986e050a8bf2951
Zilin1216 <e97716@gmail.com>
2024-12-22 15:20:21 +0800

    20241222

--- server.js
+++ server.js
@@ -2,27 +2,35 @@
 var express = require("express");
 var server = express();
 var bodyParser = require("body-parser");
+const PORT =80;
+// 載入模組
+const DB = require("nedb-promises");
+
 
 //初始化伺服器與資料庫
 server.use(express.static(__dirname+"/Portfolio"));
 server.use(bodyParser.json());
 server.use(bodyParser.urlencoded());
-
-var DB = require("nedb-promises");
+// 初始化資料庫
+const PortfolioDB = DB.create({
+  filename: __dirname + "/Portfolio.db", // 指定資料庫文件
+  autoload: true, // 自動載入資料庫
+});
 var ProfolioDB = DB.create(__dirname+"/Portfolio.db");
 
-server.get("/Portfolio", (req,res)=>{
-    //DB
-    ProfolioDB.find({}).then(results=>{
-      if(results != null){
-           res.send(results);
-      }else{
+server.get("/api/portfolio", (req, res) => {
+  ProfolioDB.find({}).then(results => {
+      if (results != null) {
+          res.json(results); // 使用 JSON 格式返回
+      } else {
           res.send("Zi-Lin's Portfolio");
       }
-    })
-})
+  });
+});
+
 
 
 server.listen(80, ()=>{
    console.log("Server is running at port 80.");
+   console.log(`Server running at http://localhost:${PORT}`);
 })
