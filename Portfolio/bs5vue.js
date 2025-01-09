1bfcf6ff074b627283b1b9300d30a4091cbc6717
Zilin1216 <e97716@gmail.com>
2025-01-09 23:46:29 +0800

    0109

---
+++ Portfolio/bs5vue.js
@@ -0,0 +1,45 @@
+var card = Vue.createApp({
+  data() {
+    return {
+      cards: [], // 初始化 cards 為空陣列
+    };
+  },
+  mounted() {
+    // 使用 jQuery 的 $.ajax 發送請求
+    $.ajax({
+      url: "/Portfolio", // API 路徑
+      method: "GET",    // HTTP 方法
+      dataType: "json", // 資料格式
+      success: (results) => {
+        this.cards = results; // 將回傳的資料綁定到 Vue 的 cards
+      },
+      error: (xhr, status, error) => {
+        console.error("Error fetching data:", status, error);
+      },
+    });
+  },
+});
+
+// 掛載 Vue 應用程式到指定的 DOM 元素
+card.component("illustration-section", {
+  template: `
+    <div class="container text-center">
+      <h2 class="text-center">Illustration</h2>
+      <div class="row row-cols-1 row-cols-md-4 p-5">
+        <div v-for="(card, index) in cards" :key="index" class="col">
+          <div class="card h-100">
+            <img :src="card.imgSrc" class="card-img-top" :alt="card.heading">
+            <div class="card-body">
+              <h5 class="card-title">{{ card.heading }}</h5>
+              <p class="card-text">{{ card.text }}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  `,
+  props: ["cards"], // 接收卡片數據
+});
+
+card.mount("#app");
+
--- server.js
+++ server.js
@@ -3,7 +3,8 @@ var express = require("express");
 var server = express();
 var bodyParser = require("body-parser");
 var fileUpload = require("express-fileupload");
-var DB = require("nedb-promises");
+var path = require("path");
+
 // 加入文件上傳中間件
 server.use(express.static(__dirname + "/Portfolio"));
 server.use(bodyParser.json());
@@ -11,27 +12,27 @@ server.use(bodyParser.urlencoded({ extended: true }));
 server.use(fileUpload({ defCharset: "utf8", defParamCharset: "utf8" }));
 
 // 設定資料庫
-var server = express();
-var portfolioDB = DB.create(__dirname + "/portfolio.db");
+var DB = require("nedb-promises");
+var PortfolioDB = DB.create(__dirname + "/portfolio.db");
 
-// 靜態資源路徑
-server.use(express.static(__dirname + "/Portfolio"));
-
-ProfolioDB.insert([
-    { modal: "card1", imgSrc: "img/project01.png", title:"窗邊的杏子", text: "Software Used: Procreate, Adobe Illustration, Photoshop" },
-    { modal: "card2", imgSrc: "img/project02.png", title:"時髦魔女", text: "Software Used: Procreate, Adobe Illustration, Photoshop" },
-    { modal: "card3", imgSrc: "img/project03.png", title:"杏子", text: "Software Used: Procreate" },
+PortfolioDB.insert([
+    { modal: "card1", imgSrc: "img/project01.png", heading:"窗邊的杏子", text: "Software Used: Procreate, Adobe Illustration, Photoshop" },
+    { modal: "card2", imgSrc: "img/project02.png", heading:"時髦魔女", text: "Software Used: Procreate, Adobe Illustration, Photoshop" },
+    { modal: "card3", imgSrc: "img/project03.png", heading:"杏子", text: "Software Used: Procreate" }
  ])
 
-// 提供 API：取得資料庫中的資料
-server.get("/api/portfolio", async (req, res) => {
-    try {
-      const newItem = await PortfolioDB.insert({newItem});
-      res.status(201).json(insertedItem); // 返回 JSON 格式的資料給前端
-    } catch (error) {
-      res.status(500).send("Error fetching data from database.");
-    }
-  });
+
+ server.get("/Portfolio", (req, res) => {
+  ProfolioDB.find({})
+    .then((results) => {
+      res.json(results); // 返回 JSON 格式的結果
+    })
+    .catch((error) => {
+      console.error("Error fetching data:", error);
+      res.status(500).send("Error fetching Portfolio data");
+    });
+});
+
   
   server.listen(3000, () => {
     console.log("Server is running on port 3000.");
