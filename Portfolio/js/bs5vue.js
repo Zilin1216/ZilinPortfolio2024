488994b3fe15740ce8604af58dad2f6a9965fdaa
Zilin1216 <e97716@gmail.com>
2025-01-10 13:21:00 +0800

    0110

--- Portfolio/js/bs5vue.js
+++ Portfolio/js/bs5vue.js
@@ -1,14 +1,14 @@
 var card = Vue.createApp({
   data() {
     return {
-      cards: [], // 儲存作品列表
+      cards: [], // 儲存從後端獲取的資料
     };
   },
   methods: {
-    // 獲取所有作品資料
+    // 獲取資料
     fetchCards() {
       $.ajax({
-        url: "/portfolio",
+        url: "/portfolio", // 從伺服器獲取資料
         method: "GET",
         dataType: "json",
         success: (results) => {
@@ -20,41 +20,17 @@ var card = Vue.createApp({
         },
       });
     },
-    // 新增作品資料
-    addCard() {
-      const newCard = {
-        modal: "card7",
-        imgSrc: "img/心城.png",
-        heading: "night",
-        text: "Software Used: Photoshop",
-      };
-
-      $.ajax({
-        url: "/portfolio/add",
-        method: "POST",
-        contentType: "application/json",
-        data: JSON.stringify(newCard),
-        success: () => {
-          console.log("New card added successfully");
-          this.fetchCards(); // 更新作品列表
-        },
-        error: (xhr, status, error) => {
-          console.error("Error adding new card:", status, error);
-        },
-      });
-    },
   },
   mounted() {
-    this.fetchCards(); // 初始化時獲取資料
+    this.fetchCards(); // 組件掛載時獲取資料
   },
 });
 
 // 定義組件
 card.component("illustration-section", {
   template: `
-    <div class="container text-center">
+    <div>
       <h2 class="text-center">Illustration</h2>
-      <button @click="$root.addCard" class="btn btn-primary mb-3">新增作品</button>
       <div class="row row-cols-1 row-cols-md-4 p-5">
         <div v-for="(item, index) in $root.cards" :key="index" class="col">
           <div class="card h-100">
--- server.js
+++ server.js
@@ -1,61 +1,61 @@
 var express = require("express");
 var server = express();
 var bodyParser = require("body-parser");
-var fileUpload = require("express-fileupload");
 var path = require("path");
 
 // 使用中間件
 server.use(express.static(__dirname + "/Portfolio"));
 server.use(bodyParser.json());
 server.use(bodyParser.urlencoded({ extended: true }));
-server.use(fileUpload({ defCharset: "utf8", defParamCharset: "utf8" }));
 
 // 設定資料庫
 var DB = require("nedb-promises");
 var PortfolioDB = DB.create(__dirname + "/portfolio.db");
 
-// 初始化資料庫（僅供測試，實際應該移除靜態插入邏輯）
-// 如果資料庫是空的，可以執行一次插入初始資料
+// 初始化資料庫（僅供測試，實際應用中可以手動插入資料）
 /*PortfolioDB.find({}).then((results) => {
   if (results.length === 0) {
     PortfolioDB.insert([
-      { modal: "card1", imgSrc: "img/project01.png", heading: "窗邊的杏子", text: "Software Used: Procreate, Adobe Illustration, Photoshop" },
-      { modal: "card2", imgSrc: "img/project02.png", heading: "時髦魔女", text: "Software Used: Procreate, Adobe Illustration, Photoshop" },
-      { modal: "card3", imgSrc: "img/project03.png", heading: "杏子", text: "Software Used: Procreate" },
+      {
+        modal: "card1",
+        imgSrc: "img/project01.png",
+        heading: "窗邊的杏子",
+        text: "Software Used: Procreate, Adobe Illustration, Photoshop",
+      },
+      {
+        modal: "card2",
+        imgSrc: "img/project02.png",
+        heading: "時髦魔女",
+        text: "Software Used: Procreate, Adobe Illustration, Photoshop",
+      },
+      {
+        modal: "card3",
+        imgSrc: "img/project03.png",
+        heading: "杏子",
+        text: "Software Used: Procreate",
+      },
     ]).then(() => console.log("Initial data inserted."));
   }
 });*/
 
-// GET 路由 - 返回所有作品資料
-server.get("/portfolio", (req, res) => {
+PortfolioDB.insert([
+  {
+    modal: "card1",
+    imgSrc: "img/project04.PNG",
+    heading: "star",
+    text: "Software Used: Procreate, Adobe Illustration, Photoshop",
+  
+  },])
+
+// API 路由 - 返回所有資料
+server.get("/portfolio", (_req, res) => {
   PortfolioDB.find({})
     .then((results) => {
-      res.json(results); // 返回 JSON 格式的結果
+      res.json(results); // 返回 JSON 格式的資料
     })
     .catch((error) => {
       console.error("Error fetching data:", error);
       res.status(500).send("Error fetching portfolio data");
-    });
-});
-
-// POST 路由 - 新增作品資料
-server.post("/portfolio/add", (req, res) => {
-  const newCard = req.body;
-
-  // 驗證資料完整性
-  if (!newCard.modal || !newCard.imgSrc || !newCard.heading || !newCard.text) {
-    return res.status(400).send("Incomplete card data");
-  }
-
-  // 插入新資料到資料庫
-  PortfolioDB.insert(newCard)
-    .then(() => {
-      console.log("New card added:", newCard);
-      res.status(200).send("New card added successfully");
-    })
-    .catch((error) => {
-      console.error("Error adding new card:", error);
-      res.status(500).send("Error adding new card");
     });
 });
 
